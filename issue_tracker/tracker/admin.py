59e18afd11521e91cfa9a069dd68a7a5ba46d53e
nrbk <nrbksv@gmail.com>
2021-03-11 11:26:18 +0600

    Models registered in the admin-panel. Admin created.

--- issue_tracker/tracker/admin.py
+++ issue_tracker/tracker/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from tracker.models import Issue, Status, Type
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ['status', 'type_issue', 'summary', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'status', 'type_issue']
+    search_fields = ['summary']
+    fields = ['id', 'status', 'type_issue', 'summary', 'description', 'created_at', 'updated_at']
+    readonly_fields = ['id', 'created_at', 'updated_at']
+
+
+admin.site.register(Issue, IssueAdmin)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['status']
+    fields = ['status']
+
+
+admin.site.register(Status, StatusAdmin)
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['type_issue']
+    fields = ['type_issue']
+
+
+admin.site.register(Type, TypeAdmin)
