c95cf5b30ef4bf8f1f58c5dd7d1fa4d4cf5da46a
nrbk <nrbksv@gmail.com>
2021-03-11 12:15:04 +0600

    Added detailed view of issue.

--- issue_tracker/issue_tracker/urls.py
+++ issue_tracker/issue_tracker/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tracker.views import IssueListView
+from tracker.views import IssueListView, IssueDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IssueListView.as_view(), name='issues-list'),
+    path('issue/<int:pk>', IssueDetail.as_view(), name='issue-detail'),
 ]
--- issue_tracker/tracker/views.py
+++ issue_tracker/tracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 
 from tracker.models import Issue, Status, Type
@@ -10,4 +10,13 @@ class IssueListView(TemplateView):
     def get_context_data(self, **kwargs):
         kwargs['issues'] = Issue.objects.all()
         return super().get_context_data(**kwargs)
+
+
+class IssueDetail(TemplateView):
+    template_name = 'issue_detail.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['issue'] = get_object_or_404(Issue, id=kwargs.get('pk'))
+        return super().get_context_data(**kwargs)
+
 
