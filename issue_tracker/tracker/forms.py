69e887ba38f60c28469501e1f019c351eef7a478
nrbk <nrbksv@gmail.com>
2021-03-15 18:39:12 +0600

    Added styles.

--- issue_tracker/tracker/forms.py
+++ issue_tracker/tracker/forms.py
@@ -7,7 +7,7 @@ from tracker.models import Issue, Status, Type
 
 class IssueForm(forms.Form):
     model = Issue
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='Статус')
-    type_issue = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=True, widget=widgets.CheckboxSelectMultiple, label='Тип')
-    summary = forms.CharField(max_length=300, required=True, label='Заголовок')
-    description = forms.CharField(max_length=3000, widget=widgets.Textarea, required=False, label='Описание')
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), empty_label=None, required=True, widget=widgets.Select(attrs={'class': 'form-control'}), label='Статус')
+    type_issue = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=True, widget=widgets.CheckboxSelectMultiple(attrs={'class': 'list-unstyled'}), label='Тип')
+    summary = forms.CharField(max_length=300, required=True, widget=widgets.TextInput(attrs={'class': 'form-control'}), label='Заголовок')
+    description = forms.CharField(max_length=3000, widget=widgets.Textarea(attrs={'class': 'form-control'}), required=False, label='Описание')
