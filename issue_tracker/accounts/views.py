3d9d173a2d9f429d37bd12c907adde4b5ba229e8
nrbk <nrbksv@gmail.com>
2021-04-07 18:50:07 +0600

    Реализовано перенаправление пользователя после авторизации на страницу, с которой был отправлен запрос.

--- issue_tracker/accounts/views.py
+++ issue_tracker/accounts/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 
 
 def login_view(request, *args, **kwargs):
     context = {}
+    next_url = request.GET.get('next')
+    if next_url is None:
+        next_url = '/projects/'
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('projects-list')
+            return HttpResponseRedirect(f'{next_url}')
         context['has_error'] = True
     return render(request, 'login.html', context=context)
 
 
-@login_required
 def logout_view(request, *args, **kwargs):
     logout(request)
     return redirect('projects-list')
