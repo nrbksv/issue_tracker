df0d3b505720edb5b2c238a35da6b92a915d3306
nrbk <nrbksv@gmail.com>
2021-04-16 18:08:28 +0600

    Добавлен просмотр профиль пользователя

--- issue_tracker/accounts/urls.py
+++ issue_tracker/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import RegisterView
+from accounts.views import RegisterView, UserDetailView
 
 
 app_name = 'accounts'
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', RegisterView.as_view(), name='register'),
+    path('user/<int:pk>/profile', UserDetailView.as_view(), name='user-profile')
 ]
--- issue_tracker/accounts/views.py
+++ issue_tracker/accounts/views.py
@@ -1,6 +1,8 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 from django.shortcuts import redirect
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.contrib.auth.models import User
 from accounts.forms import RegisterForm
 
@@ -23,3 +25,19 @@ class RegisterView(CreateView):
             next_url = 'tracker:projects-list'
         return next_url
 
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        projects = self.object.projects.all()
+        paginator = Paginator(projects, self.paginate_by)
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        kwargs['page_obj'] = page
+        kwargs['projects'] = page.object_list
+        kwargs['is_paginated'] = page.has_other_pages()
+        return super().get_context_data(**kwargs)
