e626b3e74d7808fd6f9b2faaf0a7c43bf6d8eb8e
nrbk <nrbksv@gmail.com>
2021-04-16 15:24:23 +0600

    Установлена библиотека 'Pillow'. Добавлена модель 'Profile'. Миграции сделаны.

--- issue_tracker/accounts/models.py
+++ issue_tracker/accounts/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='profile', verbose_name='Пользователь')
+    avatar = models.ImageField(blank=True, null=True, upload_to='avatars', verbose_name='Аватар')
+    git_hub = models.URLField(blank=True, null=True, verbose_name='Профиль в "GitHub"')
+    about = models.TextField(max_length=1000, blank=True, null=True, verbose_name='О себе')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        db_table = 'profiles'
+        verbose_name = 'Профиль пользователя'
+        verbose_name_plural = 'Профили пользователей'
